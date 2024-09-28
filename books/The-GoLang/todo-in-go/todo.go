5932d33a301ea4bbd137bc881ee42044740f822d
Sourav Sharma <souravsharma2468@gmail.com>
2024-09-28 12:24:19 +0530

    Go-Todo

---
+++ books/The-GoLang/todo-in-go/todo.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/aquasecurity/table"
+)
+
+type Todo struct {
+	Title        string
+	Completed    bool
+	CreatedAt    time.Time
+	CompeletedAt *time.Time
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:        title,
+		Completed:    false,
+		CompeletedAt: nil,
+		CreatedAt:    time.Now(),
+	}
+
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("Invalid index")
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+	*todos = append(t[:index], t[index+1:]...)
+	return nil
+}
+
+func (todos *Todos) toggle(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	isCompleted := t[index].Completed
+	if !isCompleted {
+		completionTime := time.Now()
+		t[index].CompeletedAt = &completionTime
+
+	}
+	t[index].Completed = !isCompleted
+	return nil
+}
+
+func (todos *Todos) edit(index int, title string) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	t[index].Title = title
+	return nil
+}
+
+func (todos *Todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "Title", "Completed", "Created At", "Completed At")
+	for index, t := range *todos {
+		completed := "❌"
+		completedAt := ""
+
+		if t.Completed {
+			completed = "✅"
+			if t.CompeletedAt != nil {
+				completedAt = t.CompeletedAt.Format(time.RFC1123)
+			}
+		}
+		table.AddRow(strconv.Itoa(index), t.Title, completed, t.CreatedAt.Format(time.RFC1123), completedAt)
+	}
+	table.Render()
+}
